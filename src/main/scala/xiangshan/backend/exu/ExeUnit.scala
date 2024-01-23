f2415b1aa0017f032fa0621563253e6871a62b49
Yang Chengyuan <yangchengyuan17@mails.ucas.ac.cn>
2024-01-23 15:22:34 +0800

    flush pipe for dasicscall to prevent jump bound errors

--- src/main/scala/xiangshan/backend/exu/ExeUnit.scala
+++ src/main/scala/xiangshan/backend/exu/ExeUnit.scala
@@ -167,6 +167,7 @@ class JumpCSRExeUnit(implicit p: Parameters) extends ExeUnit(JumpCSRExeUnitCfg){
     csr.io.in.bits.uop.ctrl.fuOpType := CSROpType.wrt
     // Receive illegal instruction exception from CSR
     io.out.bits.uop.cf.exceptionVec(illegalInstr) := csr.io.out.bits.uop.cf.exceptionVec(illegalInstr)
+    io.out.bits.uop.ctrl.flushPipe := true.B
   }
 }
 
